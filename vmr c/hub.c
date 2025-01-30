4ca20278ae502b86f822818b5cd150cc2c548d37
Capim32 <92652402+Capim32@users.noreply.github.com>
2025-01-30 12:42:44 -0300

    Atualizar o vmr c

---
+++ vmr c/hub.c
@@ -0,0 +1,26 @@
+// hub incompleto teste
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int hub(){
+
+        int escolha;
+
+        printf("1- clientes \n2- funcionarios \n3- veículos \n4- entregas \n");
+        printf("qual deseja verificar? \n");
+        scanf(":%d \n",&escolha);
+        if (escolha>1 || escolha<4){
+            printf("a");
+        }
+
+        else {
+            printf("guei");
+        }   
+
+    }
+
+return 0;
+}
