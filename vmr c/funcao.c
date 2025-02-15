95cf71058ec3214b776c8ffb418ed86c4e5caa6f
MayrlonM <mayrlon.silva2302@gmail.com>
2025-02-15 16:05:21 -0300

    Update funcao.c

--- vmr c/funcao.c
+++ vmr c/funcao.c
@@ -65,23 +65,4 @@ void visualizarClientes();
 void salvarClientes();
 void carregarClientes();
 
-int menu() {
-    int escolha;
-    printf("//- Gestão dos Correios -//\n");
-    printf("Pressione o número correspondente à opção desejada.\n");
-    printf("[1] Veículos\n");
-    printf("[2] Entregas\n");
-    printf("[3] Funcionários\n");
-    printf("[4] Clientes\n");
-
-    scanf("%d", &escolha);
-
-    switch (escolha) {
-        case 1: g_veiculos(); break;
-        case 2: g_entregas(); break;
-        case 3: g_funcionarios(); break;
-        case 4: g_clientes(); break;
-        default: printf("Opção inválida!\n");
-    }
-    return 0;
-}
+int menu();
